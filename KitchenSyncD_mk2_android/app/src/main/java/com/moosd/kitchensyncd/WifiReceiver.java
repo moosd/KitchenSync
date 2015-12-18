cc1e6e3cc6f07530f477b930e599f8d9e61ee3f0
moosd <souramoo@gmx.com>
2015-12-18 03:35:43 +0000

    Moved arpping command from tasker into app

--- KitchenSyncD_mk2_android/app/src/main/java/com/moosd/kitchensyncd/WifiReceiver.java
+++ KitchenSyncD_mk2_android/app/src/main/java/com/moosd/kitchensyncd/WifiReceiver.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import com.stericson.RootShell.execution.Command;
+import com.stericson.RootTools.RootTools;
 
 public class WifiReceiver extends BroadcastReceiver {
     public WifiReceiver() {
@@ -21,10 +23,37 @@ public class WifiReceiver extends BroadcastReceiver {
         if (isConnectedViaWifi(context)) {
             try {
                 Thread.sleep(5000);
+                // announce presence and update everyone's arp cache
+                Command command = new Command(0, "/data/nmap/busybox arping -q -c 3 -U -I wlan0 $(ip addr show dev wlan0|grep 'inet '|cut -d' ' -f6|cut -d'/' -f1)") {
+                    boolean dostuff = false;
+                    @Override
+                    public void commandOutput(int id, String line) {
+                        super.commandOutput(id, line);
+                    }
+
+                    @Override
+                    public void commandTerminated(int id, String reason) {
+                        super.commandTerminated(id, reason);
+                        if(!dostuff) {
+                            dostuff = true;
+                            Intent pushIntent = new Intent(context, BackgroundService.class);
+                            context.startService(pushIntent);
+                        }
+                    }
+
+                    @Override
+                    public void commandCompleted(int id, int exitcode) {
+                        super.commandCompleted(id, exitcode);
+                        if(!dostuff) {
+                            dostuff = true;
+                            Intent pushIntent = new Intent(context, BackgroundService.class);
+                            context.startService(pushIntent);
+                        }
+                    }
+                };
+                RootTools.getShell(true).add(command);
             } catch (Exception e) {
             }
-            Intent pushIntent = new Intent(context, BackgroundService.class);
-            context.startService(pushIntent);
         }
     }
 }
